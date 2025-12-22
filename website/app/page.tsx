8e343f1a46a45d7ed97698a10445edf9a635cc2d
Ravindra Lakkireddy <85186928+FafnirX26@users.noreply.github.com>
2025-12-22 10:47:01 -0500

    Removed CredibilitySection from the Home component.
    Removed CredibilitySection from the Home component.

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import TeamSection from "@/components/TeamSection";
-import CredibilitySection from "@/components/CredibilitySection";
 import Reveal from "@/components/Reveal";
 import MoleculeInterfaceLoader from "@/components/MoleculeInterfaceLoader";
 
@@ -12,9 +11,6 @@ export default function Home() {
       </Reveal>
       <Reveal delayMs={80} once={false}>
         <TeamSection />
-      </Reveal>
-      <Reveal delayMs={120} once={false}>
-        <CredibilitySection />
       </Reveal>
       <Reveal delayMs={160} once={false}>
         <MoleculeInterfaceLoader />
